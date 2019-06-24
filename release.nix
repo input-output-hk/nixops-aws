8aeae92264da5fb75858a6ded4437d37dec43a27
John Lotoski <john.lotoski@iohk.io>
2019-06-24 11:01:53 -0400

    Clean up release.nix

--- release.nix
+++ release.nix
@@ -12,8 +12,7 @@ rec {
   build = pkgs.lib.genAttrs [ "x86_64-linux" "i686-linux" "x86_64-darwin" ] (system:
     with import nixpkgs { inherit system; };
 
-#   python2Packages.buildPythonApplication rec {
-    python2Packages.buildPythonPackage rec {
+    python2Packages.buildPythonApplication rec {
       name = "nixops-aws-${version}";
       namePrefix = "";
 
@@ -26,6 +25,7 @@ rec {
       '';
 
       buildInputs = [ python2Packages.nose python2Packages.coverage ];
+
 
       propagatedBuildInputs = with python2Packages;
         [
@@ -41,21 +41,12 @@ rec {
 
       doCheck = true;
 
-#      postCheck = ''
-#        # We have to unset PYTHONPATH here since it will pick enum34 which collides
-#        # with python3 own module. This can be removed when nixops is ported to python3.
-#        PYTHONPATH= mypy --cache-dir=/dev/null nixops
-#
-#        # smoke test
-#        HOME=$TMPDIR $out/bin/nixops --version
-#      '';
+      # Needed by libcloud during tests
+      SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
 
-#      # Needed by libcloud during tests
-#      SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-
-#      # Add openssh to nixops' PATH. On some platforms, e.g. CentOS and RHEL
-#      # the version of openssh is causing errors when have big networks (40+)
-#      makeWrapperArgs = ["--prefix" "PATH" ":" "${openssh}/bin" "--set" "PYTHONPATH" ":"];
+      # Add openssh to nixops' PATH. On some platforms, e.g. CentOS and RHEL
+      # the version of openssh is causing errors when have big networks (40+)
+      makeWrapperArgs = ["--prefix" "PATH" ":" "${openssh}/bin" "--set" "PYTHONPATH" ":"];
 
       postInstall =
         ''
@@ -63,11 +54,7 @@ rec {
           cp -av nix/* $out/share/nix/nixops-aws
         '';
 
-#      meta.description = "Nix package for ${stdenv.system}";
-    });
-
-#  tests.none_backend = (import ./tests/none-backend.nix {
-#    nixops = build.x86_64-linux;
-#    system = "x86_64-linux";
-#  }).test;
+      meta.description = "Nix package for ${stdenv.system}";
+    }
+  );
 }
