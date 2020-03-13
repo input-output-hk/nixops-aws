f2a249f90d0f04c72447ea47cfc5531f038cdecd
adisbladis <adisbladis@gmail.com>
2020-03-13 23:21:51 +0000

    Migrate to poetry2nix

---
+++ default.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+
+  overrides = import ./overrides.nix { inherit pkgs; };
+
+in pkgs.poetry2nix.mkPoetryApplication {
+  projectDir = ./.;
+  overrides = pkgs.poetry2nix.overrides.withDefaults(overrides);
+}
