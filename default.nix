766391bec28f17154461223980c5a034defb02f3
Michael Fellinger <michael.fellinger@iohk.io>
2020-04-24 13:43:31 +0200

    copy nix files to /share

--- default.nix
+++ default.nix
@@ -7,4 +7,9 @@ let
 in pkgs.poetry2nix.mkPoetryApplication {
   projectDir = ./.;
   overrides = pkgs.poetry2nix.overrides.withDefaults overrides;
+
+  postInstall = ''
+    mkdir -p $out/share/nix/nixopsaws
+    cp -av nix/* $out/share/nix/nixopsaws
+  '';
 }
