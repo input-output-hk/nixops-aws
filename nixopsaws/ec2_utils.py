9e5933f46867647e5dd4e3e191ad0e6443e5b02f
Shay Bergmann <sbergmann@kreisys.co>
2020-11-20 14:14:25 -0500

    ... same for old boto

--- nixopsaws/ec2_utils.py
+++ nixopsaws/ec2_utils.py
@@ -66,9 +66,10 @@ def fetch_aws_secret_key(access_key_id):
 def connect(region, access_key_id):
     """Connect to the specified EC2 region using the given access key."""
     assert region
-    (access_key_id, secret_access_key) = fetch_aws_secret_key(access_key_id)
-    conn = boto.ec2.connect_to_region(
-        region_name=region, aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+    # (access_key_id, secret_access_key) = fetch_aws_secret_key(access_key_id)
+    # conn = boto.ec2.connect_to_region(
+    #     region_name=region, aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+    conn = boto.ec2.connect_to_region(region)
     if not conn:
         raise Exception("invalid EC2 region ‘{0}’".format(region))
     return conn
